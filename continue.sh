01b71c3b4570ce76692d9b9abcb242c44535b663
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2023-03-20 10:11:50 +0100

    Add success message to the script

--- continue.sh
+++ continue.sh
@@ -7,3 +7,5 @@ fi
 
 sudo rm "$CERTBOT_CERT_PATH/live/$DOMAIN/privkey.pem"
 sudo rm "$CERTBOT_CERT_PATH/live/$DOMAIN/fullchain.pem"
+
+echo "The script will continuing with the certbot configuration."
