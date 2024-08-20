131e63e4eed41394984eeed9b5dcd0cda34f2f17
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2024-08-20 23:36:25 +0200

    Update deploy script to save docker container name first time

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,27 @@
 
 PROJECT_NAME=$1
 
+# get project name from file
+if [ -f .project ] && [ -z "$PROJECT_NAME" ]
+then
+  PROJECT_NAME_FILE=$(cat .project)
+  if [ ! -z "$PROJECT_NAME_FILE" ]
+  then
+    PROJECT_NAME=$PROJECT_NAME_FILE
+  fi
+fi
+
 if [ -z "$PROJECT_NAME" ]
 then
   echo "ERROR: Empty project name!"
   echo "Ex: ./deploy.sh <project-name>"
   exit 1
+else
+  # save project name in file
+  echo $PROJECT_NAME > .project
 fi
 
 echo "Starting project $PROJECT_NAME with docker"
 
 echo "Docker container name: $PROJECT_NAME";
-docker-compose --project-name=$PROJECT_NAME up -d --build
+#docker-compose --project-name=$PROJECT_NAME up -d --build
